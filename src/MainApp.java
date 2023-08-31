452835a57d661ef8b3db1e8e962582cdba2d9c13
R3ddy95 <sup3r3do95@gmail.com>
2023-08-31 23:50:21 +0200

    Development v0.1

---
+++ src/MainApp.java
@@ -0,0 +1,10 @@
+package app;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // ...
+    }
+}
---
+++ src/app/Vendita.java
@@ -0,0 +1,3 @@
+public class Vendita {
+    // Campi, costruttore, metodi getter e setter
+}
